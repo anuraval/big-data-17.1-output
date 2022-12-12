4471339fad3d9d051332d2314959701f4e9af6a6
anuraval <119819815+anuraval@users.noreply.github.com>
2022-12-11 22:38:53 -0600

    Add files via upload

---
+++ python 17.1 output.py
@@ -0,0 +1,56 @@
+Anushka Raval
+
+>>>
+
+------PART 1------
+Authors' last names
+0	   Wald
+1	  Quirk
+2	 Deitel
+3	 Deitel
+4	 Deitel
+------PART 2------
+Book titles
+                             title
+0	 Android 6 for Programmers
+1	    Android How to Program
+2	          C How to Program
+3	        C++ How to Program
+4    Internet & WWW How to Program
+5    Intro to Python for CS and DS
+6	       Java How to Program
+7 Visual Basic 2012 How to Program
+8	  Visual C# How to Program
+9	 Visual C++ How to Program
+------PART 3------
+INNER JOIN to select all the books
+       id	first	 last	                       title    copyright	   isbn
+0	2      Harvey  Deitel	   Android 6 for Programmers	     2016    0134289366
+1	2      Harvey  Deitel  Intro to Python for CS and DS	     2020    0135404673
+2	2      Harvey  Deitel  Internet & WWW How to Program	     2012    0132151006
+3	2      Harvey  Deitel		 Java How to Program	     2018    0134743350
+4	2      Harvey  Deitel		    C How to Program	     2016    0133976890
+------PART 4------
+Insert a new author
+         first	  last
+0	  Paul	Deitel
+1	Harvey	Deitel
+2	 Abbey	Deitel
+3	   Dan	 Quirk
+4    Alexander	  Wald
+5	Robert Jackson
+------PART 5------	
+Insert a new title for an author	
+                 isbn				  title	  edition   copyright
+0	   0135404673	  Intro to Python for CS and DS	        1	 2020
+1	   0132151006	  Internet & WWW How to Program	        5	 2012
+2	   0134743350	            Java How to Program	       11   	 2018
+3	   0133976890	               C How to Program	        8	 2016
+4	   0133406954  Visual Basic 2012 How to Program  	6	 2014
+5	   0134601548	       Visual C# How to Program 	6	 2017
+6	   0136151574	      Visual C++ How to Program 	2	 2008
+7	   0134448235		     C++ How to Program        10	 2017
+8	   0134444302		 Android How to Program	        3	 2017
+9	   0134289366	      Android 6 for Programmers 	3	 2016
+10	9781305117204	     System Analysis and Design 	7	 2016
+>>>				
